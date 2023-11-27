e96aa0390356b030b5484325b9439a2f5503be05
Shohboz Xurramov <shohboztuit@gmail.com>
2023-11-28 00:22:06 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            string path = "https://www.kun.com";
+            var result = await DownloadDataAsync(path);
+
+            Console.WriteLine("Natija:" + result);
+        }
+        static async Task<string> DownloadDataAsync(string path)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(path);
+            var response = await client.GetStringAsync("");
+            
+            
+            return response;
+        }
+    }
+}
